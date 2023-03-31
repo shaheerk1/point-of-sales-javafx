f45c70757de813890f95cf2cada7751fe46aa925
shaheer <mhmmdmsk@gmail.com>
2023-03-31 19:37:22 +0530

    lombok added

--- src/main/java/lk/target/dto/EmployeeDTO.java
+++ src/main/java/lk/target/dto/EmployeeDTO.java
@@ -1,44 +1,23 @@
 package lk.target.dto;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
 public class EmployeeDTO {
     private String id;
     private String title;
     private String name;
     private String address;
     private String mobile;
-
-    public EmployeeDTO(String id, String title, String name, String address, String mobile, String city, String province, Double salary, String role, String username, String password) {
-        this.id = id;
-        this.title = title;
-        this.name = name;
-        this.address = address;
-        this.mobile = mobile;
-        this.city = city;
-        this.province = province;
-        this.salary = salary;
-        this.role = role;
-        this.username = username;
-        this.password = password;
-    }
-
     private String city;
     private String province;
-    private Double salary;
+    private double salary;
     private String role;
-
-    public EmployeeDTO(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
     private String username;
     private String password;
 
--- src/main/java/lk/target/model/EmployeeModel.java
+++ src/main/java/lk/target/model/EmployeeModel.java
@@ -15,7 +15,19 @@ public class EmployeeModel {
         ResultSet rs = CrudUtil.execute(sql,username);
 
         if (rs.next()){
-            EmployeeDTO emp = new EmployeeDTO(rs.getString(10), rs.getNString(11));
+            EmployeeDTO emp = new EmployeeDTO(
+                    rs.getString(1),
+                    rs.getString(2),
+                    rs.getString(3),
+                    rs.getString(4),
+                    rs.getString(5),
+                    rs.getString(6),
+                    rs.getString(7),
+                    rs.getDouble(8),
+                    rs.getString(9),
+                    rs.getString(10),
+                    rs.getNString(11)
+            );
             return emp;
         }else {
             return null;
